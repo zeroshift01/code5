a7ec147c60697e40c7363c8636635c7aa457ac4f
seuk <zeroshift01@naver.com>
2021-01-15 09:31:19 +0900

    code5 DAY8  FIN

--- code5_8/src/com/code5/biz/emp/emp002/Emp002.java
+++ code5_8/src/com/code5/biz/emp/emp002/Emp002.java
@@ -1,6 +1,8 @@
 package com.code5.biz.emp.emp002;
 
+import com.code5.fw.data.Box;
 import com.code5.fw.web.BizController;
+import com.code5.fw.web.BoxContext;
 
 /**
  * @author seuk
@@ -14,6 +16,12 @@ public class Emp002 implements BizController {
 	 * @throws Exception
 	 */
 	public String emp00201() throws Exception {
+
+		Box box = BoxContext.getThread();
+		if (box.getBoolean("err")) {
+			box.setAlertMsg("오류가 발생했습니다.");
+			throw new Exception();
+		}
 
 		return "emp00201";
 	}
