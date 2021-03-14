948143a19e7c51dfd2b38d88d351a2a3a46fa66a
zero <zero@zero>
2021-03-14 13:11:25 +0900

    FIN

---
+++ inflearn05/src/com/code5/biz/login/Login.java
@@ -0,0 +1,86 @@
+package com.code5.biz.login;
+
+import com.code5.fw.data.Box;
+import com.code5.fw.data.SessionB;
+import com.code5.fw.data.Table;
+import com.code5.fw.web.BizController;
+import com.code5.fw.web.BoxContext;
+import com.code5.fw.web.MasterController;
+
+/**
+ * @author zero
+ *
+ */
+public class Login implements BizController {
+
+	/**
+	 * @return
+	 * @throws Exception
+	 */
+	public String login() throws Exception {
+
+		Box box = BoxContext.getThread();
+
+		String ID = box.s("ID");
+		String PIN = box.s("PIN");
+
+		LoginD dao = LoginD.getLoginD();
+
+		Table user = dao.login_01();
+
+		if (user.size() != 1) {
+			box.put("ret", "아이디가 없거나 패스워드가 일치하지 않습니다.");
+			return MasterController.execute("loginView");
+		}
+
+		Box thisUser = user.getBox();
+
+		if (!PIN.equals(thisUser.s("PIN"))) {
+			box.put("ret", "아이디가 없거나 패스워드가 일치하지 않습니다.");
+
+			if (dao.login_02() != 1) {
+				throw new Exception();
+			}
+
+			return MasterController.execute("loginView");
+		}
+
+		int FAIL_CNT = thisUser.getInt("FAIL_CNT");
+		if (FAIL_CNT > 5) {
+			box.put("ret", "패스워드를 5회 이상 실패하였습니다.");
+			return MasterController.execute("loginView");
+		}
+
+		if (dao.login_03() != 1) {
+			throw new Exception();
+
+		}
+
+		String AUTH = thisUser.s("AUTH");
+		String IP = box.s(Box.KEY_REMOTE_ADDR);
+
+		SessionB sessionB = new SessionB(ID, AUTH, IP);
+		box.setSessionB(sessionB);
+
+		return MasterController.execute("emp00101");
+	}
+
+	/**
+	 * @return
+	 * @throws Exception
+	 */
+	public String logout() throws Exception {
+
+		BoxContext.getThread().setSessionB(null);
+		return loginView();
+	}
+
+	/**
+	 * @return
+	 * @throws Exception
+	 */
+	public String loginView() throws Exception {
+		return "loginView";
+	}
+
+}
---
+++ inflearn05/src/com/code5/fw/data/Box.java
@@ -0,0 +1,212 @@
+package com.code5.fw.data;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+/**
+ * @author zero
+ *
+ */
+public abstract class Box implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @param key
+	 * @param object
+	 * 
+	 *
+	 */
+	public abstract void put(String key, Object object);
+
+	/**
+	 * @param key
+	 * @return
+	 * 
+	 * 
+	 */
+	public abstract Object get(String key);
+
+	/**
+	 * @param key
+	 * @return
+	 * 
+	 * 
+	 */
+	public String getString(String key) {
+
+		Object s = get(key);
+
+		if (s == null) {
+			return "";
+		}
+
+		if (!(s instanceof String)) {
+			return "";
+
+		}
+		return (String) s;
+	}
+
+	/**
+	 * @param key
+	 * @param defaultx
+	 * @return
+	 * 
+	 * 
+	 */
+	public String getString(String key, String defaultx) {
+
+		String s = getString(key);
+
+		if ("".equals(s)) {
+			return defaultx;
+		}
+
+		return s;
+
+	}
+
+	/**
+	 * @param key
+	 * @return
+	 * 
+	 * 
+	 */
+	public String s(String key) {
+		return getString(key);
+	}
+
+	/**
+	 * @param key
+	 * @param defualtx
+	 * @return
+	 * 
+	 * 
+	 */
+	public String s(String key, String defualtx) {
+		return getString(key, defualtx);
+	}
+
+	/**
+	 * @param key
+	 * @return
+	 * 
+	 * 
+	 */
+	public int getInt(String key) {
+
+		String s = s(key);
+		if ("".equals(s)) {
+			return 0;
+		}
+
+		try {
+
+			return Integer.parseInt(s);
+
+		} catch (Exception ex) {
+
+			return 0;
+		}
+
+	}
+
+	/**
+	 * @param key
+	 * @return
+	 * 
+	 * 
+	 */
+	public BigDecimal getBigDecimal(String key) {
+
+		String s = s(key);
+		if ("".equals(s)) {
+			return BigDecimal.ZERO;
+		}
+
+		try {
+
+			return new BigDecimal(s);
+
+		} catch (Exception ex) {
+
+			return BigDecimal.ZERO;
+		}
+
+	}
+
+	/**
+	 * @param key
+	 * @return
+	 */
+	public Table getTable(String key) {
+
+		Object obj = get(key);
+		if (obj == null) {
+			return new Table();
+		}
+
+		if (obj instanceof Table) {
+			return (Table) obj;
+
+		}
+
+		return new Table();
+
+	}
+
+	/**
+	 * @param key
+	 * @return
+	 */
+	public boolean getBoolean(String key) {
+
+		String s = s(key);
+		if ("true".equals(s)) {
+			return true;
+		}
+
+		return false;
+
+	}
+
+	/**
+	 * @param key
+	 * @param b
+	 * @return
+	 */
+	public void put(String key, boolean b) {
+		put(key, "" + b);
+	}
+
+	/**
+	 * 
+	 */
+	public static String KEY_SESSIONB = "com.code5.fw.web.KEY_SESSIONB";
+
+	/**
+	 * 
+	 */
+	public static String KEY_REMOTE_ADDR = "com.code5.fw.web.KEY_REMOTE_ADDR";
+
+	/**
+	 * 
+	 */
+	public static String KEY_SERVICE_KEY = "com.code5.fw.web.KEY_SERVICE_KEY";
+
+	/**
+	 * @param sessionB
+	 */
+	public abstract void setSessionB(SessionB sessionB);
+
+	/**
+	 * @return
+	 */
+	public SessionB getSessionB() {
+		return (SessionB) get(KEY_SESSIONB);
+	}
+}
