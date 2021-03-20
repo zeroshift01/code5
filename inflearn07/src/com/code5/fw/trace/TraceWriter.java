a96c07b9dffb2e800f9d8231c7dd3fbc718759b2
zero <zero@zero>
2021-03-20 14:26:38 +0900

    f

---
+++ inflearn07/src/com/code5/fw/trace/TraceWriter.java
@@ -0,0 +1,98 @@
+package com.code5.fw.trace;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+
+/**
+ * @author seuk
+ *
+ */
+class TraceWriter {
+
+	String logKey = null;
+	String logFileUrl = null;
+
+	boolean isMulti = false;
+
+	int initCnt = 0;
+	int errCnt = 0;
+
+	private BufferedWriter out = null;
+
+	/**
+	 * @param logKey
+	 * @param logFileUrl
+	 * @param isMulti
+	 */
+	TraceWriter(String logKey, String logFileUrl, boolean isMulti) {
+
+		this.logKey = logKey;
+		this.logFileUrl = logFileUrl;
+		this.isMulti = isMulti;
+
+		try {
+
+			this.out = new BufferedWriter(new FileWriter(this.logFileUrl, true));
+
+		} catch (Exception ex) {
+			ex.printStackTrace();
+
+		}
+	}
+
+	/**
+	 * @param log
+	 */
+	void println(String log) {
+
+		if (this.out == null) {
+			return;
+		}
+
+		try {
+			this.out.write(log);
+			this.out.newLine();
+
+			if (!isMulti) {
+				this.out.flush();
+			}
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+
+	/**
+	 * 
+	 */
+	void close() {
+
+		if (this.out == null) {
+			return;
+		}
+
+		try {
+			this.out.flush();
+			this.out.close();
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+
+	}
+
+	/**
+	 * 
+	 */
+	void flush() {
+
+		if (this.out == null) {
+			return;
+		}
+
+		try {
+			this.out.flush();
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+
+	}
+}
