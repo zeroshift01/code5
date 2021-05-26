e501898ad2eceee2901a2a1c7da4b6a8fade80f8
zero <zero@zero>
2021-05-26 23:37:02 +0900

    CODE5

--- inflearn08/src_fw/com/code5/fw/web/RunCode5.java
+++ inflearn08/src_fw/com/code5/fw/web/RunCode5.java
@@ -1,5 +1,7 @@
 package com.code5.fw.web;
 
+import java.net.Socket;
+
 import org.apache.catalina.startup.Tomcat;
 
 import com.code5.fw.data.InitYaml;
@@ -18,8 +20,15 @@ public class RunCode5 {
 			return;
 		}
 
+		init.setAppName(RunCode5.class.getName());
+
 		String baseDir = init.s("RUN_CODE5.BASE_DIR");
 		int webPort = Integer.parseInt(init.s("RUN_CODE5.WEB_PORT"));
+
+		if (!isWebPort(webPort)) {
+			System.out.println(webPort + " 는 사용중입니다.");
+			return;
+		}
 
 		String webappDir = init.s("RUN_CODE5.WEB_APP_DIR");
 
@@ -30,7 +39,41 @@ public class RunCode5 {
 		tomcat.addWebapp("", webappDir);
 
 		tomcat.start();
+
+		System.out.println("        (__) ");
+		System.out.println("        (oo) ");
+		System.out.println(" /-------\\/ ");
+		System.out.println("/ |     ||   ");
+		System.out.println("* ||----||   ");
+		System.out.println("  ~~    ~~   ");
+		System.out.println("  code5 start ");
+
 		tomcat.getServer().await();
 
 	}
+
+	/**
+	 * @param webPort
+	 * @return
+	 */
+	private static boolean isWebPort(int webPort) throws Exception {
+
+		Socket socket = null;
+		try {
+			socket = new Socket("localhost", webPort);
+			socket.setSoTimeout(1);
+			if (socket.isConnected()) {
+				return false;
+			}
+			return true;
+		} catch (Exception ex) {
+			return true;
+		} finally {
+			if (socket != null) {
+				socket.close();
+			}
+		}
+
+	}
+
 }
