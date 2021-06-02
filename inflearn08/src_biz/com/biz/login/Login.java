c13fae575f73f55884fffff8fb859e99b9c26b62
zero <zero@zero>
2021-06-02 20:43:41 +0900

    ff

--- inflearn08/src_biz/com/biz/login/Login.java
+++ inflearn08/src_biz/com/biz/login/Login.java
@@ -3,7 +3,6 @@ package com.biz.login;
 import com.code5.fw.data.Box;
 import com.code5.fw.data.SessionB;
 import com.code5.fw.data.Table;
-import com.code5.fw.security.CryptPin;
 import com.code5.fw.web.BizController;
 import com.code5.fw.web.BoxContext;
 import com.code5.fw.web.ServiceAnnotation;
@@ -23,24 +22,22 @@ public class Login implements BizController {
 
 		Box box = BoxContext.getThread();
 
-		String ID = box.s("ID");
-		String PIN = box.s("PIN");
-
-		PIN = CryptPin.cryptPin(PIN, ID);
-
 		LoginD dao = LoginD.getLoginD();
 
 		Table user = dao.login_01();
 
 		if (user.size() != 1) {
-			box.put("ret", "아이디가 없거나 패스워드가 일치하지 않습니다.");
+			box.setAlertMsg("아이디가 없거나 패스워드가 일치하지 않습니다.");
 			return execute("callLogin");
 		}
 
 		Box thisUser = user.getBox();
 
-		if (!PIN.equals(thisUser.s("PIN"))) {
-			box.put("ret", "아이디가 없거나 패스워드가 일치하지 않습니다.");
+		String INPUT_PIN = thisUser.s("INPUT_PIN");
+		String PIN = thisUser.s("PIN");
+
+		if (!INPUT_PIN.equals(PIN)) {
+			box.setAlertMsg("아이디가 없거나 패스워드가 일치하지 않습니다.");
 
 			if (dao.login_02() != 1) {
 				throw new Exception();
@@ -51,14 +48,16 @@ public class Login implements BizController {
 
 		int FAIL_CNT = thisUser.getInt("FAIL_CNT");
 		if (FAIL_CNT > 5) {
-			box.put("ret", "패스워드를 5회 이상 실패하였습니다.");
+			box.setAlertMsg("패스워드를 5회 이상 실패하였습니다.");
 			return execute("callLogin");
 		}
 
+		String ID = thisUser.s("ID");
 		String AUTH = thisUser.s("AUTH");
 		String IP = box.s(Box.KEY_REMOTE_ADDR);
 
 		SessionB sessionB = box.getSessionB();
+
 		sessionB.set(ID, AUTH, IP);
 		box.setSessionB(sessionB);
 
