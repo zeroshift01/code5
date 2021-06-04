df83fe6923053019a6cc47f0124d1dbd2c57ba35
skmpr <skmpr@zero>
2021-06-04 12:52:38 +0900

    f

--- inflearn02/src_fw/com/code5/fw/data/YamlReader.java
+++ inflearn02/src_fw/com/code5/fw/data/YamlReader.java
@@ -1,5 +1,6 @@
 package com.code5.fw.data;
 
+import java.io.File;
 import java.io.InputStream;
 import java.util.Map;
 
@@ -10,6 +11,33 @@ import org.yaml.snakeyaml.Yaml;
  *
  */
 public class YamlReader {
+
+	public static void main(String[] xxx) throws Exception {
+
+		ClassLoader cl = YamlReader.class.getClassLoader();
+
+		String yamlName = "init.yaml";
+
+		String path = cl.getResource(yamlName).getPath();
+
+		// init.yaml
+		File file = new File(path);
+
+		// classes/init.yaml
+		file = file.getParentFile();
+
+		// WEB-INF/classes/init.yaml
+		file = file.getParentFile();
+
+		// web/WEB-INF/classes/init.yaml
+		file = file.getParentFile();
+
+		// root/web/WEB-INF/classes/init.yaml
+		file = file.getParentFile();
+
+		System.out.println(file.getAbsolutePath());
+
+	}
 
 	/**
 	 * @param n
@@ -23,12 +51,18 @@ public class YamlReader {
 
 			ClassLoader cl = YamlReader.class.getClassLoader();
 
+			yamlName = yamlName.replaceAll("\\.", "\\/");
+
 			yamlName = yamlName + ".yaml";
 
 			inputStream = cl.getResourceAsStream(yamlName);
 
 			Yaml yaml = new Yaml();
 			Map<String, Object> map = yaml.load(inputStream);
+
+			String path = cl.getResource(yamlName).getPath();
+			map.put("THIS_YAML_URL", path);
+
 			return map;
 
 		} catch (Exception ex) {
