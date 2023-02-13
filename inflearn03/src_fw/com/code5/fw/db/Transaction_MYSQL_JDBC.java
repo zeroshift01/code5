2326362dbb0584ac0cb9db3422d07158a619b030
zero <zero@DESKTOP-5ELHQ3S>
2023-02-13 15:11:57 +0900

    MySQL 추가

---
+++ inflearn03/src_fw/com/code5/fw/db/Transaction_MYSQL_JDBC.java
@@ -0,0 +1,36 @@
+package com.code5.fw.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ * @author zero
+ *
+ */
+public class Transaction_MYSQL_JDBC extends Transaction {
+
+	static {
+
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+
+	/**
+	 *
+	 */
+	protected Connection createConnection() throws SQLException {
+
+		String url = "jdbc:mysql://10.1.1.1:11223/mysql";
+		String id = "id";
+		String pin = "pin";
+
+		Connection conn = DriverManager.getConnection(url, id, pin);
+		return conn;
+
+	}
+
+}
